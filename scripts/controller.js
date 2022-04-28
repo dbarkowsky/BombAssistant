e2529542047d4846d649c3acd92a5ab321817317
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2022-04-28 13:41:15 -0700

    Initial commit. Just basic layout.

---
+++ scripts/controller.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#canvas").html = `<div class="row">
+                            <div class="col-sm-3 module-panel">1</div>
+                            <div class="col-sm-3 module-panel">2</div>
+                            <div class="col-sm-3 module-panel">3</div>
+                            <div class="col-sm-3 module-panel">4</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-3 module-panel">5</div>
+                            <div class="col-sm-3 module-panel">6</div>
+                            <div class="col-sm-3 module-panel">7</div>
+                            <div class="col-sm-3 module-panel">8</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-3 module-panel">9</div>
+                            <div class="col-sm-3 module-panel">10</div>
+                            <div class="col-sm-3 module-panel">11</div>
+                            <div class="col-sm-3 module-panel">12</div>
+                        </div>`;
+    console.log("Bomb modules in canvas.");
+});
