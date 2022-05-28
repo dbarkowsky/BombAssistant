ee9972d52f4ad1a44c5a0bfa835204400bfdf113
Kiereek <dylanbarkowsky@gmail.com>
2022-05-28 14:51:09 -0700

    Working simple-wires.

--- scripts/bomb.js
+++ scripts/bomb.js
@@ -13,19 +13,56 @@ class Bomb{
         this.parallelPort = false;
 
         $(".even-odd-button").on("click", this.evenOddButtons);
+        $(".vowel-button").on("click", this.serialVowelButton);
     }
 
     evenOddButtons(){
         console.log(`Bomb: evenOddButtons() - ${this.id}`);
 
-        //set any buttons with this class to default
-        $(".selected").removeClass("selected");
+        // Set any buttons with this class to default
+        $(".selected.even-odd-button").removeClass("selected");
 
+        // Add the class back to the appropriate button
         $(this).addClass("selected");
 
+        // Assign even or odd boolean based on id
         if ($(this).attr("id") == "even")
             this.serialEven = true;
         else
             this.serialEven = false;
+    }
+
+    serialVowelButton(){
+        console.log(`Bomb: serialVowelButton() - ${this.value}`);
+
+        // Set any buttons with this class to default
+        $(".selected.vowel-button").removeClass("selected");
+
+        // Add the class back to the appropriate button
+        $(this).addClass("selected");
+
+        // Assign even or odd boolean based on id
+        if ($(this).attr("value") == 1)
+            this.serialVowel = true;
+        else
+            this.serialVowel = false;
+    }
+
+    batteryCountButtons(){
+        // Check if up or down button is pressed
+        // Increment or decrement value appropriately
+        // Update visual
+    }
+
+    litIndicatorButtons(){
+        // Check if button pressed is already in the list
+        // If so, remove selected style and remove from list
+        // If not, add selected style and push to list
+    }
+
+    parallelPortButton(){
+        // Check if button is already marked as selected
+        // If so, removed selected and change to false
+        // Otherwise, add selected and change to true
     }
 }
--- scripts/simple-wires.js
+++ scripts/simple-wires.js
@@ -1,10 +1,6 @@
-class SimpleWires{
-    serial;
-    wireCount;
-
+class SimpleWires{    
     constructor (){
         this.draw();
-        this.serial = null;
     }
 
     draw(){
@@ -37,6 +33,11 @@ class SimpleWires{
         const RED_INDEX = 2;
         const WHITE_INDEX = 3;
         const BLACK_INDEX = 4;
+        const YELLOW_RGB = 'rgb(255, 255, 0)';
+        const BLUE_RGB = 'rgb(0, 0, 255)';
+        const RED_RGB = 'rgb(255, 0, 0)';
+        const WHITE_RGB = 'rgb(245, 245, 245)';
+        const BLACK_RGB = 'rgb(0, 0, 0)';
 
         let coloursCount = [0, 0, 0, 0, 0];
         const rawWires = document.getElementsByClassName("wire");
@@ -45,38 +46,95 @@ class SimpleWires{
         for (let wire of rawWires) {
             if (wire.style.visibility == "visible"){
                 refinedWires.push(wire);
-            }
-
-            switch (wire.style.backgroundColor){
-                case 'rgb(255, 0, 0)':
-                    coloursCount[RED_INDEX]++;
-                    break;
-                case 'rgb(245, 245, 245)':
-                    coloursCount[WHITE_INDEX]++;
-                    break;
-                case 'rgb(0, 0, 0)':
-                    coloursCount[BLACK_INDEX]++;
-                    break;
-                case 'rgb(255, 255, 0)':
-                    coloursCount[YELLOW_INDEX]++;
-                    break;
-                case 'rgb(0, 0, 255)':
-                    coloursCount[BLUE_INDEX]++;
-                    break;
+            
+                switch (wire.style.backgroundColor){
+                    case RED_RGB:
+                        coloursCount[RED_INDEX]++;
+                        break;
+                    case WHITE_RGB:
+                        coloursCount[WHITE_INDEX]++;
+                        break;
+                    case BLACK_RGB:
+                        coloursCount[BLACK_INDEX]++;
+                        break;
+                    case YELLOW_RGB:
+                        coloursCount[YELLOW_INDEX]++;
+                        break;
+                    case BLUE_RGB:
+                        coloursCount[BLUE_INDEX]++;
+                        break;
+                }
             }
         }
 
         console.log("SimpleWires: setCommands() - " + refinedWires.length);
         console.log("SimpleWires: setCommands() - " + coloursCount);
+        const LAST_WIRE = refinedWires.length - 1;
         switch (refinedWires.length){
             case 3:
-                $("#commands").html(`3 wires.`);
+                // If there are no red wires, cut the second wire.
+                if (coloursCount[RED_INDEX] == 0)
+                    $("#commands").html(`Cut the second wire.`);
+                // If the last wire is white, cut the last wire.
+                else if (refinedWires[LAST_WIRE].style.backgroundColor == WHITE_RGB)
+                    $("#commands").html(`Cut the last wire.`);
+                // If there is more than one blue wire, cut the last blue wire.
+                else if (coloursCount[BLUE_INDEX] > 1)
+                    $("#commands").html(`Cut the last blue wire.`);
+                // Cut the last wire.
+                else
+                    $("#commands").html(`Cut the last wire.`);
                 break;
             case 4:
+                // If there is > 1 red wire and last digit of serial is odd, cut the last red wire.
+                if (coloursCount[RED_INDEX] > 1 && 
+                    $('#odd').hasClass('selected'))
+                    $("#commands").html(`Cut the last red wire.`);
+                // If the last wire is yellow and there are no red wires, cut the first wire.
+                else if (refinedWires[LAST_WIRE].style.backgroundColor == YELLOW_RGB &&
+                        coloursCount[RED_INDEX] === 0)
+                    $("#commands").html(`Cut the first wire.`);
+                // If there is only 1 blue wire, cut the first wire.
+                else if (coloursCount[BLUE_INDEX] === 1)
+                    $("#commands").html(`Cut the first wire.`);
+                // If there is > 1 yellow wire, cut the last wire.
+                else if (coloursCount[YELLOW_INDEX] > 1)
+                    $("#commands").html(`Cut the last wire.`);
+                // Cut the second wire
+                else
+                    $("#commands").html(`Cut the second wire.`);
                 break;
             case 5:
+                // If the last wire is black and the last digit of serial is odd, cut 4th wire.
+                if (refinedWires[LAST_WIRE].style.backgroundColor == BLACK_RGB &&
+                    $('#odd').hasClass('selected'))
+                    $("#commands").html(`Cut the fourth wire.`);
+                // If there is only 1 red wire and > 1 yellow wire, cut the first wire.
+                else if (coloursCount[RED_INDEX] === 1 &&
+                        coloursCount[YELLOW_INDEX] > 1)
+                    $("#commands").html(`Cut the first wire.`);
+                // If there are 0 black wires, cut the second wire.
+                else if (coloursCount[BLACK_INDEX] === 0)
+                    $("#commands").html(`Cut the second wire.`);
+                // Cut the first wire.
+                else 
+                    $("#commands").html(`Cut the first wire.`);
                 break;
             case 6:
+                // If there are 0 yellow wires, and last digit of serial is odd, cut 3rd wire.
+                if (coloursCount[YELLOW_INDEX] === 0 &&
+                    $('#odd').hasClass('selected'))
+                    $("#commands").html(`Cut the third wire.`);
+                // If there is only 1 yellow wire and > 1 white wire, cut 4th wire.
+                else if (coloursCount[YELLOW_INDEX] === 1 &&
+                        coloursCount[WHITE_INDEX] > 1)
+                    $("#commands").html(`Cut the fourth wire.`);
+                // If there are 0 red wires, cut the last wire.
+                else if (coloursCount[RED_INDEX] === 0)
+                    $("#commands").html(`Cut the last wire.`);
+                // Cut the 4th wire.
+                else
+                    $("#commands").html(`Cut the fourth wire.`);
                 break;
             default:
                 $("#commands").html(`Add more wires.`);
