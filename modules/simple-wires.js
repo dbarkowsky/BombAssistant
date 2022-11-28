f53b305f8c84daad087b2782c7cc1cb837b3a4b8
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2022-11-27 22:23:57 -0800

    big simple wires change

--- modules/simple-wires.js
+++ modules/simple-wires.js
@@ -1,20 +1,39 @@
+const YELLOW_INDEX = 0;
+const BLUE_INDEX = 1;
+const RED_INDEX = 2;
+const WHITE_INDEX = 3;
+const BLACK_INDEX = 4;
+const YELLOW_RGB = 'rgb(255, 255, 0)';
+const BLUE_RGB = 'rgb(0, 0, 255)';
+const RED_RGB = 'rgb(255, 0, 0)';
+const WHITE_RGB = 'rgb(245, 245, 245)';
+const BLACK_RGB = 'rgb(0, 0, 0)';
+
 class SimpleWires {
+  wiresDisplayed;
+  wiresVisible;
+  wireList;
+  coloursCount;
+
   constructor() {
     this.draw();
     this.setSettings();
+    this.wiresVisible = 1;
+    this.wireList = [];
+    this.coloursCount = [];
   }
 
   // TODO: change implementation so only one row is present at a time + clearing wire removes row
   draw() {
     console.log('SimpleWires.draw(): drawing in canvas');
     $('#canvas')
-      .html(` <div class="row console" id="instructions">Instructions appear here.</div>
+      .html(` <div class="row console" id="instructions">Select wires matching bomb module.</div>
                             <div class="steel-plate container-fluid" id="simple-wire-container">
                               <div id="top-left-text">Serial: </div>
                             </div>
                             <div class="row console" id="commands">Add more wires.</div>`);
     for (let i = 0; i < 6; i++) {
-      $('#simple-wire-container').append(`<div class="row simple-wire-row p-1">
+      $('#simple-wire-container').append(`<div class="row simple-wire-row p-1 ${i > 0 ? "hidden" : ""}" id="${i}">
                                 <div class="col-4">
                                     <div class="wire my-auto"></div>
                                 </div>
@@ -29,87 +48,79 @@ class SimpleWires {
                             </div>`);
     }
     $('.colour-button').on('click', this.clickedButton);
-    $('.colour-button').on('click', this.setCommands);
   }
 
-  // Checks wires and runs logic to give defuser commands
-  setCommands() {
-    const YELLOW_INDEX = 0;
-    const BLUE_INDEX = 1;
-    const RED_INDEX = 2;
-    const WHITE_INDEX = 3;
-    const BLACK_INDEX = 4;
-    const YELLOW_RGB = 'rgb(255, 255, 0)';
-    const BLUE_RGB = 'rgb(0, 0, 255)';
-    const RED_RGB = 'rgb(255, 0, 0)';
-    const WHITE_RGB = 'rgb(245, 245, 245)';
-    const BLACK_RGB = 'rgb(0, 0, 0)';
-
+  // Collects info on wires.
+  collectInfo = () => {
     // Get the wires
-    let coloursCount = [0, 0, 0, 0, 0];
+    this.coloursCount = [0, 0, 0, 0, 0];
     const rawWires = document.getElementsByClassName('wire');
-    let refinedWires = [];
+    this.wireList = [];
 
     // For each wire, add to list and count colours
     for (let wire of rawWires) {
       if (wire.style.visibility == 'visible') {
-        refinedWires.push(wire);
+        this.wireList.push(wire);
 
         switch (wire.style.backgroundColor) {
           case RED_RGB:
-            coloursCount[RED_INDEX]++;
+            this.coloursCount[RED_INDEX]++;
             break;
           case WHITE_RGB:
-            coloursCount[WHITE_INDEX]++;
+            this.coloursCount[WHITE_INDEX]++;
             break;
           case BLACK_RGB:
-            coloursCount[BLACK_INDEX]++;
+            this.coloursCount[BLACK_INDEX]++;
             break;
           case YELLOW_RGB:
-            coloursCount[YELLOW_INDEX]++;
+            this.coloursCount[YELLOW_INDEX]++;
             break;
           case BLUE_RGB:
-            coloursCount[BLUE_INDEX]++;
+            this.coloursCount[BLUE_INDEX]++;
             break;
         }
       }
     }
+    this.wiresVisible = this.wireList.length;
+  }
 
-    console.log('SimpleWires: setCommands() - ' + refinedWires.length);
-    console.log('SimpleWires: setCommands() - ' + coloursCount);
+  // Checks wires and runs logic to give defuser commands
+  setCommands = () => {
+    // console.log('SimpleWires: setCommands() - ' + this.wireList.length);
+    // console.log('SimpleWires: setCommands() - ' + this.coloursCount);
     // Track last wire for logic purposes
-    const LAST_WIRE = refinedWires.length - 1;
+    const LAST_WIRE = this.wireList.length - 1;
 
     // Run logic for which command to give
-    switch (refinedWires.length) {
+    switch (this.wireList.length) {
       case 3:
         // If there are no red wires, cut the second wire.
-        if (coloursCount[RED_INDEX] == 0)
+        if (this.coloursCount[RED_INDEX] == 0)
           $('#commands').html(`Cut the second wire.`);
         // If the last wire is white, cut the last wire.
-        else if (refinedWires[LAST_WIRE].style.backgroundColor == WHITE_RGB)
+        else if (this.wireList[LAST_WIRE].style.backgroundColor == WHITE_RGB)
           $('#commands').html(`Cut the last wire.`);
         // If there is more than one blue wire, cut the last blue wire.
-        else if (coloursCount[BLUE_INDEX] > 1)
+        else if (this.coloursCount[BLUE_INDEX] > 1)
           $('#commands').html(`Cut the last blue wire.`);
         // Cut the last wire.
         else $('#commands').html(`Cut the last wire.`);
         break;
       case 4:
         // If there is > 1 red wire and last digit of serial is odd, cut the last red wire.
-        if (coloursCount[RED_INDEX] > 1 && $('#odd').hasClass('selected'))
+        if (this.coloursCount[RED_INDEX] > 1 && $('#odd').hasClass('selected'))
           $('#commands').html(`Cut the last red wire.`);
         // If the last wire is yellow and there are no red wires, cut the first wire.
         else if (
-          refinedWires[LAST_WIRE].style.backgroundColor == YELLOW_RGB &&
-          coloursCount[RED_INDEX] === 0
+          this.wireList[LAST_WIRE].style.backgroundColor == YELLOW_RGB &&
+          this.coloursCount[RED_INDEX] === 0
         )
           $('#commands').html(`Cut the first wire.`);
         // If there is only 1 blue wire, cut the first wire.
-        else if (coloursCount[BLUE_INDEX] === 1)
+        else if (this.coloursCount[BLUE_INDEX] === 1)
           $('#commands').html(`Cut the first wire.`);
         // If there is > 1 yellow wire, cut the last wire.
-        else if (coloursCount[YELLOW_INDEX] > 1)
+        else if (this.coloursCount[YELLOW_INDEX] > 1)
           $('#commands').html(`Cut the last wire.`);
         // Cut the second wire
         else $('#commands').html(`Cut the second wire.`);
@@ -117,34 +128,34 @@ class SimpleWires {
       case 5:
         // If the last wire is black and the last digit of serial is odd, cut 4th wire.
         if (
-          refinedWires[LAST_WIRE].style.backgroundColor == BLACK_RGB &&
+          this.wireList[LAST_WIRE].style.backgroundColor == BLACK_RGB &&
           $('#odd').hasClass('selected')
         )
           $('#commands').html(`Cut the fourth wire.`);
         // If there is only 1 red wire and > 1 yellow wire, cut the first wire.
         else if (
-          coloursCount[RED_INDEX] === 1 &&
-          coloursCount[YELLOW_INDEX] > 1
+          this.coloursCount[RED_INDEX] === 1 &&
+          this.coloursCount[YELLOW_INDEX] > 1
         )
           $('#commands').html(`Cut the first wire.`);
         // If there are 0 black wires, cut the second wire.
-        else if (coloursCount[BLACK_INDEX] === 0)
+        else if (this.coloursCount[BLACK_INDEX] === 0)
           $('#commands').html(`Cut the second wire.`);
         // Cut the first wire.
         else $('#commands').html(`Cut the first wire.`);
         break;
       case 6:
         // If there are 0 yellow wires, and last digit of serial is odd, cut 3rd wire.
-        if (coloursCount[YELLOW_INDEX] === 0 && $('#odd').hasClass('selected'))
+        if (this.coloursCount[YELLOW_INDEX] === 0 && $('#odd').hasClass('selected'))
           $('#commands').html(`Cut the third wire.`);
         // If there is only 1 yellow wire and > 1 white wire, cut 4th wire.
         else if (
-          coloursCount[YELLOW_INDEX] === 1 &&
-          coloursCount[WHITE_INDEX] > 1
+          this.coloursCount[YELLOW_INDEX] === 1 &&
+          this.coloursCount[WHITE_INDEX] > 1
         )
           $('#commands').html(`Cut the fourth wire.`);
         // If there are 0 red wires, cut the last wire.
-        else if (coloursCount[RED_INDEX] === 0)
+        else if (this.coloursCount[RED_INDEX] === 0)
           $('#commands').html(`Cut the last wire.`);
         // Cut the 4th wire.
         else $('#commands').html(`Cut the fourth wire.`);
@@ -155,22 +166,25 @@ class SimpleWires {
   }
 
   // When a user clicks a colour button
-  clickedButton() {
+  clickedButton = () => {
     // Get button colour
-    let colour = $(this).css('background-color');
+    let colour = $(event.target).css('background-color');
     // Get related wire/line
-    let line = $(this).closest('.simple-wire-row').find('.wire');
+    let line = $(event.target).closest('.simple-wire-row').find('.wire');
 
     console.log('SimpleWires: clickedButton() - ' + colour);
 
     // If already clear, add colour and show
-    if (!$(this).hasClass('clear')) {
+    if (!$(event.target).hasClass('clear')) {
       line.css('background-color', colour);
       line.css('visibility', 'visible');
     } else {
       // else, hide it
       line.css('visibility', 'hidden');
     }
+    this.collectInfo();
+    this.setCommands();
+    this.showWires(this.wiresVisible);
   }
 
   // Handles modal for requesting settings
@@ -215,6 +229,34 @@ class SimpleWires {
     $(event.target).addClass('selected');
     // And the global settings equivalent (remove temp from id)
     $(`#${event.target.id.substring(5)}`).addClass('selected');
+  }
+
+  // Sees which wires should be visible and makes it so
+  showWires = (visibleWireCount) => {
+    console.log('visiblewirecount', visibleWireCount);
+    const allWires = document.querySelectorAll('.wire');
+    console.log('allWires', allWires);
+    console.log('wireList', this.wireList);
+    allWires.forEach((wire, index) => {
+      if (this.wireList[index]){
+        console.log('wire background', wire.style.backgroundColor);
+        console.log('wirelist value', this.wireList[index].style.backgroundColor)
+        console.log('wirelist index', index);
+        wire.style.backgroundColor = this.wireList[index].style.backgroundColor;
+        wire.classList.remove('hidden');
+        wire.style.visibility = 'visible';
+      } else {
+        wire.classList.add('hidden');
+        wire.style.visibility = 'hidden';
+      }
+    })
+
+    const allRows = document.querySelectorAll('.simple-wire-row');
+    allRows.forEach(row => {
+      if (row.id <= visibleWireCount){ row.classList.remove('hidden'); }
+      else { row.classList.add('hidden'); }
+    });
+    
   }
 
   // Updates the Serial notification
